3e2acb97fdee1db8f93446c980d97834762d5f90
JuanIgnacioMarchetto <tu@email.com>
2024-04-16 09:51:50 -0300

    16/04/2024-09:51

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [dueDate, setDueDate] = useState(new Date());
+  const [alarmTime, setAlarmTime] = useState(null);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (inputValue.trim() !== '') {
+      const newTask = {
+        task: inputValue,
+        dueDate: dueDate,
+        alarmTime: alarmTime ? new Date(alarmTime) : null // Convertir a objeto Date si se establece el tiempo de alarma
+      };
+      setTasks([...tasks, newTask]);
+      setInputValue('');
+      setDueDate(new Date());
+      setAlarmTime(null);
+      alert('Tarea agregada exitosamente');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const isTaskOverdue = (dueDate) => {
+    const now = new Date();
+    return dueDate < now;
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      tasks.forEach((task, index) => {
+        if (isTaskOverdue(task.dueDate)) {
+          alert(`La tarea "${task.task}" está vencida`);
+        }
+      });
+    }, 60000); 
+
+    return () => clearInterval(interval);
+  }, [tasks]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1>Lista de Tareas</h1>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Escribe una tarea..."
+          />
+          <DatePicker selected={dueDate} onChange={(date) => setDueDate(date)} showTimeSelect />
+          <input
+            type="time"
+            value={alarmTime || ''}
+            onChange={(event) => setAlarmTime(event.target.value)}
+          />
+          <button onClick={handleAddTask}>Agregar</button>
+          <ul>
+            {tasks.map((task, index) => (
+              <li key={index}>
+                {task.task} - {task.dueDate.toString()}
+                {isTaskOverdue(task.dueDate) && <span style={{ color: 'red' }}> - Vencida</span>}
+                <button onClick={() => handleDeleteTask(index)}>Eliminar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
