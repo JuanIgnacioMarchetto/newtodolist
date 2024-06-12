c615cabbce642a88642d66570827788ec734b292
Juan Ignacio Marchetto <43477348+JuanIgnacioMarchetto@users.noreply.github.com>
2024-06-12 08:33:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import DatePicker from 'react-datepicker';
+import React, { useState, useEffect } from 'react'; 
+import DatePicker from 'react-datepicker'; // componente de DatePicker
 import 'react-datepicker/dist/react-datepicker.css';
-import './App.css';
+import './App.css'; 
+import { format } from 'date-fns'; //  para formatear fechas
 
 function App() {
+  // estado inicial de tareas
   const [tasks, setTasks] = useState(() => {
     const storedTasks = localStorage.getItem('tasks');
     return storedTasks ? JSON.parse(storedTasks) : [];
   });
-  const [inputValue, setInputValue] = useState('');
-  const [dueDate, setDueDate] = useState(new Date());
-  const [alarmTime, setAlarmTime] = useState(null);
+  
+  const [inputValue, setInputValue] = useState(''); // estado para el valor de input de tarea
+  const [dueDate, setDueDate] = useState(new Date()); // estado para vemcimiento
+  const [alarmTime, setAlarmTime] = useState(null); // estado alarma
 
+  //cambio de valor de input de la tarea
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
+  //nueva tarea
   const handleAddTask = () => {
     if (inputValue.trim() !== '') {
       const newTask = {
         task: inputValue,
         dueDate: dueDate,
-        alarmTime: alarmTime ? new Date(alarmTime) : null,
-        completed: false // Nuevo campo para marcar si la tarea está completada
+        alarmTime: alarmTime ? new Date(`${format(dueDate, 'yyyy-MM-dd')}T${alarmTime}`) : null,
+        completed: false //marca tarea cumplida
       };
-      const updatedTasks = [...tasks, newTask];
-      setTasks(updatedTasks);
-      localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+      const updatedTasks = [...tasks, newTask]; // agrega la nueva tarea a la lista 
+      setTasks(updatedTasks); // actualiza el estado de las tareas
+      localStorage.setItem('tasks', JSON.stringify(updatedTasks)); // guarda en localStorage
       setInputValue('');
       setDueDate(new Date());
-      setAlarmTime(null);
+      setAlarmTime(null); 
       alert('Tarea agregada exitosamente');
     }
   };
 
+  // elimina tareass
   const handleDeleteTask = (index) => {
     const newTasks = [...tasks];
-    newTasks.splice(index, 1);
-    setTasks(newTasks);
+    newTasks.splice(index, 1); 
+    setTasks(newTasks); 
     localStorage.setItem('tasks', JSON.stringify(newTasks));
   };
 
+//reìte proceso de confirmacione detarea cumplida
   const handleTaskCompletion = (index) => {
     const updatedTasks = [...tasks];
     updatedTasks[index].completed = true;
-    setTasks(updatedTasks);
-    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    setTasks(updatedTasks); 
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks)); 
   };
 
+  //chequea si una tarea esta vencida
   const isTaskOverdue = (dueDate) => {
     const now = new Date();
-    return dueDate < now;
+    return new Date(dueDate) < now;
   };
 
+  // cada 60 seg verifica vencimiento de tareas
   useEffect(() => {
     const interval = setInterval(() => {
       tasks.forEach((task, index) => {
@@ -76,25 +85,24 @@ function App() {
             onChange={handleInputChange}
             placeholder="Escribe una tarea..."
           />
-          <DatePicker selected={dueDate} onChange={(date) => setDueDate(date)} showTimeSelect />
+          <DatePicker selected={dueDate} onChange={(date) => setDueDate(date)} showTimeSelect /> 
           <input
             type="time"
             value={alarmTime || ''}
-            onChange={(event) => setAlarmTime(event.target.value)}
+            onChange={(event) => setAlarmTime(event.target.value)} // input  tiempo de alarma
           />
-          <button className="button" onClick={handleAddTask}>Agregar</button>
-          <ul>
+          <button className="button" onClick={handleAddTask}>Agregar</button> 
             {tasks.map((task, index) => (
               <li key={index}>
-                {task.task} - {task.dueDate.toString()}
+                {task.task} - {format(new Date(task.dueDate), 'PPpp')}
                 {isTaskOverdue(task.dueDate) && !task.completed && (
-                  <span style={{ color: 'red' }}> - Vencida</span>
+                  <span style={{ color: 'red' }}> - Vencida</span> //muestra si la tarea esta vencida
                 )}
-                {task.completed && <span style={{ color: 'green' }}> - Cumplida</span>}
+                {task.completed && <span style={{ color: 'green' }}> - Cumplida</span>} // mustra si la tarea esta cumplida
                 {!task.completed && (
-                  <button className="button" onClick={() => handleTaskCompletion(index)}>Confirmar Cumplida</button>
+                  <button className="button" onClick={() => handleTaskCompletion(index)}>Confirmar Cumplida</button> // boton tarea cumplida
                 )}
-                <button className="button" onClick={() => handleDeleteTask(index)}>Eliminar</button>
+                <button className="button" onClick={() => handleDeleteTask(index)}>Eliminar</button> // boton  eliminar tarea
               </li>
             ))}
           </ul>
